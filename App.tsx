8b80c96a7caca2b98375015243455b1338fc4838
jsalinas1616 <jsalinas@odintec.com.mx>
2021-11-02 14:17:03 -0600

    finish

--- App.tsx
+++ App.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React from 'react'
 import { SafeAreaView, StatusBar } from 'react-native';
-import { CalculadoraScreen } from './src/screen/CalculadoraScreen';
+import { CalculadoraScreen } from './src/screens/CalculadoraScreen';
 
 import { styles } from './src/theme/appTheme';
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.fondo}>
-      <StatusBar
-        backgroundColor='black'
+    <SafeAreaView style={ styles.fondo }>
+      <StatusBar 
+        backgroundColor="black"
         barStyle='light-content'
       />
+
       <CalculadoraScreen />
     </SafeAreaView>
   )
 }
+
 
 export default App;
